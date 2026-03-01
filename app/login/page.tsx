f26c5ece7219c4d1fe46589a6c2a40ee9dce7dd5
Ashish <ashishgoyat.official@gmail.com>
2026-03-01 09:19:51 +0530

    build landing page

---
+++ app/login/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { EncryptedText } from "@/components/ui/encrypted-text";
+import Link from "next/link";
+
+export default function LoginForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+      <h2 className="text-4xl font-extrabold text-neutral-800 dark:text-neutral-200 text-center my-20">
+        <EncryptedText
+          text="Login here."
+          encryptedClassName="text-neutral-500"
+          revealedClassName="dark:text-white text-black"
+          revealDelayMs={50}
+        />
+      </h2>
+
+      <form className="my-8" onSubmit={handleSubmit}>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+
+        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-full">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Login &rarr;
+          </span>
+          <BottomGradient />
+        </button>
+
+        <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+
+        <div className="flex flex-col space-y-4">
+          <p className="text-center text-sm text-neutral-600 dark:text-neutral-400">
+            Don't have an account?{" "}
+            <Link
+              href="/signup"
+              className="font-semibold text-neutral-800 dark:text-white hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex w-full flex-col space-y-2", className)}>
+      {children}
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,155 @@
-import Image from "next/image";
+import React from "react";
+import { DottedGlowBackground } from "@/components/ui/dotted-glow-background";
+import { CometCard } from "@/components/ui/comet-card";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconCoin,
+  IconHome,
+  IconInfoCircle,
+  IconLogin,
+  IconUserPlus,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+
+export default function DottedGlowBackgroundDemoSecond() {
+
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/",
+    },
+
+    {
+      title: "Login",
+      icon: (
+        <IconLogin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/login",
+    },
+    {
+      title: "Signup",
+      icon: (
+        <IconUserPlus className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/signup",
+    },
+    {
+      title: "About Us",
+      icon: (
+        <IconInfoCircle className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "/about",
+    },
+    {
+      title: "Pricing",
+      icon: (
+        <IconCoin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://github.com/ashishgoyat",
+    },
+  ];
+
+  const words = [
+    {
+      text: "BotForge."
+    }
+  ]
+
+  return (<>
+    <div className="flex items-center justify-between w-full px-8 pt-15">
+      <div className="flex items-center  flex-1">
+        <TypewriterEffect words={words} />
+      </div>
+      <div className="flex items-center flex-1">
+        <FloatingDock
+          items={links}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
+    <div className="relative mx-auto flex w-full max-w-7xl items-center justify-center">
+      <DottedGlowBackground
+        className="pointer-events-none mask-radial-to-90% mask-radial-at-center opacity-20 dark:opacity-100"
+        opacity={1}
+        gap={10}
+        radius={1.6}
+        colorLightVar="--color-neutral-500"
+        glowColorLightVar="--color-neutral-600"
+        colorDarkVar="--color-neutral-500"
+        glowColorDarkVar="--color-sky-800"
+        backgroundOpacity={0}
+        speedMin={0.3}
+        speedMax={1.6}
+        speedScale={1}
+      />
+
+      <div className="relative z-10 flex w-full flex-col items-center justify-between space-y-6 px-8 py-16 text-center md:flex-row">
+        <div>
+          <h2 className="text-center text-4xl font-normal tracking-tight text-neutral-900 sm:text-5xl md:text-left dark:text-neutral-400">
+            Ready to build{" "}
+            <span className="font-bold dark:text-white">AI Chatbots</span>?
+          </h2>
+          <p className="mt-4 max-w-lg text-center text-base text-neutral-600 md:text-left dark:text-neutral-300">
+            Upload your docs, FAQs, or product info. BotForge trains an AI chatbot that answers your customers instantly — and embed it on any website in seconds.
+          </p>
+          <div className="mt-4">
+            <Link href={"/signup"} className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                Start Building for Free ...
+              </span>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <CometCard>
+        <button
+          type="button"
+          className="my-10 flex w-80 cursor-pointer flex-col items-stretch rounded-[16px] border-0 bg-[#1F2121] p-2 saturate-0 md:my-20 md:p-4"
+          aria-label="View invite F7RA"
+          style={{
+            transformStyle: "preserve-3d",
+            transform: "none",
+            opacity: 1,
+          }}
+        >
+          <div className="mx-2 flex-1">
+            <div className="relative mt-2 aspect-[3/4] w-full">
+              <img
+                loading="lazy"
+                className="absolute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
+                alt="Invite background"
+                src="/vercel.svg"
+                width={300}
+                height={400}
+                style={{
+                  boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 6px 0px",
+                  opacity: 1,
+                }}
+              />
+            </div>
+          </div>
+          <div className="mt-2 flex flex-shrink-0 items-center justify-between p-4 font-mono text-white">
+            <div className="text-xs">
+
+            </div>
+            <div className="text-xs text-gray-300 opacity-50">BotForge</div>
+          </div>
+        </button>
+      </CometCard>
+    </div>
+  </>
   );
 }
